3d27112cfa181b4a22217e77a78101ad76441754
SIRI-bit-tech <siritech001@gmail.com>
2025-03-06 12:39:26 +0000

    Fix relative imports for utils in UI components

--- frontend/app/components/ui/input.tsx
+++ frontend/app/components/ui/input.tsx
@@ -1,10 +1,8 @@
 import * as React from "react"
 
-import { cn } from "@/app/lib/utils"
+import { cn } from "../../lib/utils"
 
-export interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
-
-const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type, ...props }, ref) => {
+const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(({ className, type, ...props }, ref) => {
   return (
     <input
       type={type}
@@ -20,4 +18,3 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(({ className, type,
 Input.displayName = "Input"
 
 export { Input }
-
